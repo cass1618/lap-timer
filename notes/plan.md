4bf2fa8b664675095ee99e364baa852041c4a9ad
cass1618 <cass16180@gmail.com>
2021-08-03 21:32:19 -0700

    :memo: Add MVP and plan

---
+++ notes/plan.md
@@ -0,0 +1,11 @@
+## MVP
+Your app should have these basic functions:
+1. A 1/100th of a second timer (10 millisecond per tick)
+1. User should be able to start/stop/reset the timer
+1. User should be able to record time difference between laps
+
+
+### TIMER
+1. Need some sort of time import
+1. 3 buttons: Start, stop, reset
+1. 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,13 +7,13 @@ const Home: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Lap Timer</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
+            <IonTitle size="large">Lap Timer (2nd title)</IonTitle>
           </IonToolbar>
         </IonHeader>
         <ExploreContainer />
